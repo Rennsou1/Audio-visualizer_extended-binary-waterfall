746dc7de281cf5c94bf16fcd03f97b35f7ca8f61
Unai Domínguez <unai55@outlook.es>
2025-02-19 19:26:42 +0100

    Fix subfile logger when no emoji icon is present in a subfile.

--- Generator.cs
+++ Generator.cs
@@ -256,7 +256,7 @@ public class Generator
 		{
 			foreach (var sf in _subfiles)
 			{
-				Logger.Debug($"\t{sf.IconString} '{sf.Path}' {sf.StartOffset:X8}–{sf.EndOffset:X8}");
+				Logger.Debug($"\t{sf.IconString ?? "–"} '{sf.Path}' {sf.StartOffset:X8}–{sf.EndOffset:X8}");
 			}
 		}
 	}
